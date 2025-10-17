0a15714690eced692178d78237e3206a80b0c613
alieslambeigi-blip <ali.eslambeigi@gmail.com>
2025-10-17 22:57:01 +0330

    Create vision.js

---
+++ pages/api/vision.js
@@ -0,0 +1,18 @@
+import vision from "@google-cloud/vision";
+
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: "google-vision-key.json",
+});
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  const { image } = req.body;
+
+  const [result] = await client.labelDetection({
+    image: { content: image },
+  });
+
+  const labels = result.labelAnnotations.map((label) => label.description);
+  res.status(200).json({ labels });
+}
